aff839218db90ed04a3cfa5c0e49f5792788f3f6
070035266 <070035266@109R030573Z9Q22.EDU.YRDSB.NET>
2017-11-23 11:59:35 -0500

    insertion

--- src/sortingassignment/SortingAssignment.java
+++ src/sortingassignment/SortingAssignment.java
@@ -17,5 +17,28 @@ public class SortingAssignment {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public static Comparable[] insertion(Comparable[] data){
+        Comparable[] sorted = null;
+        int high;
+        //starting at the second element, and moving to the end of the array,
+        //insert the element at i into its proper postition
+        for(int i=1; i<data.length; i++){
+            //starting at the element to the left of the inserted, moving back
+            //towards the begginning of the array
+            for(int j=i-1; j>0;j--){
+                Comparable temp = data[i];
+                //if the element being inserted is less than the element at this
+                //location, swap their locations.
+                if(temp.compareTo(data[j]) <0){
+                    //move it to the right
+                    data[j+1] = data[j];
+                //otherwise, leave the elements in their place, and move on
+                }else{
+                    data[j+1]= temp;
+                    break;
+                }
+            }
+        }
+        return sorted;
+    }
 }
