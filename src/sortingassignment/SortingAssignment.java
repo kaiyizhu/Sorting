830062370020c5f1ea2b2b257b68fbf23766a992
070035266 <070035266@109R030573Z9Q22.EDU.YRDSB.NET>
2017-11-27 12:05:49 -0500

    Merge orgin/master

--- src/sortingassignment/SortingAssignment.java
+++ src/sortingassignment/SortingAssignment.java
@@ -39,14 +39,15 @@ public class SortingAssignment
         //insert the element at i into its proper postition
         for (int i = 1; i < data.length; i++)
         {
+            Comparable inserted = data[i];
             //starting at the element to the left of the inserted, moving back
             //towards the begginning of the array
             for (int j = i - 1; j >= 0; j--)
             {
-                Comparable temp = data[i];
+                
                 //if the element being inserted is less than the element at this
                 //location, swap their locations.
-                if (temp.compareTo(data[j]) < 0)
+                if (inserted.compareTo(data[j]) < 0)
                 {
                     //move it to the right
                     data[j + 1] = data[j];
@@ -54,7 +55,7 @@ public class SortingAssignment
                 } else
                 {
                     //leave the elements in their place, and move on
-                    data[j + 1] = temp;
+                    data[j] = inserted;
                     break;
                 }
             }
